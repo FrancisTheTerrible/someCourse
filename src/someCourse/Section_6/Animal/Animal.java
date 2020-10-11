71ef6949f4c7147f49afe4b8b79f79514c0fcd69
Frantisek <frantisek.hruza@gmail.com>
2020-10-11 14:50:39 +0200

    added Animal and moved out  section into gitignore

---
+++ src/someCourse/Section_6/Animal/Animal.java
@@ -0,0 +1,65 @@
+package Section_6.Animal;
+
+public class Animal {
+    private String name;
+    private int brain;
+    private int body;
+    private int size;
+    private int weight;
+
+    public void eat(){
+        System.out.println("This dog eats something");
+    }
+
+    public void move(int speed){
+        System.out.println("This dog moves at : " + speed);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getBrain() {
+        return brain;
+    }
+
+    public void setBrain(int brain) {
+        this.brain = brain;
+    }
+
+    public int getBody() {
+        return body;
+    }
+
+    public void setBody(int body) {
+        this.body = body;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public void setSize(int size) {
+        this.size = size;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+
+    public Animal(String name, int brain, int body, int size, int weight) {
+        this.name = name;
+        this.brain = brain;
+        this.body = body;
+        this.size = size;
+        this.weight = weight;
+    }
+}
--- src/someCourse/Section_6/Main.java
+++ src/someCourse/Section_6/Main.java
@@ -1,20 +1,28 @@
 package Section_6;
 
+import Section_6.Animal.Animal;
+import Section_6.Animal.Dog;
+
 public class Main {
     public Main() {
     }
 
     public static void main(String[] args) {
-        ComplexNumber one = new ComplexNumber(1.0D, 1.0D);
-        ComplexNumber number = new ComplexNumber(2.5D, -1.5D);
-        one.add(1.0D, 1.0D);
-        System.out.println("one.real= " + one.getReal());
-        System.out.println("one.imaginary= " + one.getImaginary());
-        one.subtract(number);
-        System.out.println("one.real= " + one.getReal());
-        System.out.println("one.imaginary= " + one.getImaginary());
-        number.subtract(one);
-        System.out.println("number.real= " + number.getReal());
-        System.out.println("number.imaginary= " + number.getImaginary());
+        //ComplexNumber one = new ComplexNumber(1.0D, 1.0D);
+        //ComplexNumber number = new ComplexNumber(2.5D, -1.5D);
+        //one.add(1.0D, 1.0D);
+        //System.out.println("one.real= " + one.getReal());
+        //System.out.println("one.imaginary= " + one.getImaginary());
+        //one.subtract(number);
+        //System.out.println("one.real= " + one.getReal());
+        //System.out.println("one.imaginary= " + one.getImaginary());
+        //number.subtract(one);
+        //System.out.println("number.real= " + number.getReal());
+        //System.out.println("number.imaginary= " + number.getImaginary());
+        Animal animal = new Animal("itsName",1,1,5,5);
+        Dog dog = new Dog("Yorkie",8,20,2,4,1,20,"long silky");
+        dog.eat();
+        dog.run();
+        //dog.walk();
     }
 }
